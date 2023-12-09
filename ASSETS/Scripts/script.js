33ff159f55fb9b591978930f5e5d2cd898c9a1e6
Benske4 <128804040+Benske4@users.noreply.github.com>
2023-12-09 15:27:01 +1300

    list working update

--- ASSETS/Scripts/script.js
+++ ASSETS/Scripts/script.js
@@ -1,3 +1,5 @@
+// Section to allow user to pick a meal from the dropdown menu on pages such as sweets, beef etc BEGINS
+
 function redirectToPage(form) {
   // Get the selected option from the dropdown
   let selectedOption = form.sweets.options[form.sweets.selectedIndex];
@@ -7,4 +9,20 @@ function redirectToPage(form) {
     // Redirect to the URL specified in the selected option's value
     window.location.href = selectedOption.value;
   }
+}
+
+// Section to allow user to pick a meal from the dropdown menu on pages such as sweets, beef etc ENDS
+
+// meal planner section to allow user to add to list BEGINS
+let listItems = document.querySelector(".list-item"); // Use the correct selector for the list items
+let inputValue = document.getElementById("meal-planner-input"); // Use the correct selector for the input field
+
+function addToList() {
+  let newListItem = document.createElement("li");
+  newListItem.innerText = inputValue.value;
+
+  listItems.appendChild(newListItem);
+
+  // Clear the input field
+  inputValue.value = "";
 }
